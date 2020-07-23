2f128320521460a03a20b1990d9508be36290c93
Ruth Menezes <ruthmenezesbr@gmail.com>
2020-07-22 22:14:49 -0300

    entrega cliente

--- rede_social/app/src/opcao.js
+++ rede_social/app/src/opcao.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Opcao(props) {
     return (
         <div>
-            <img src={props.icone}/>
+            <img src={props.icone} alt=""/>
             <p>{props.nome}</p>
         </div>
     );
